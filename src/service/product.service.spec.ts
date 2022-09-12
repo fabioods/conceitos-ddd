78e88698ad502290e820d24c6efc67ce36547c51
Fabio dos santos <fah_ds@live.com>
2022-09-12 19:53:56 -0300

    feat: add product service

---
+++ src/service/product.service.spec.ts
@@ -0,0 +1,14 @@
+import { Product } from '../entity/product';
+import { ProductService } from './productService';
+
+describe('Product service test unit tests', () => {
+  it('should change the prices of all products', () => {
+    const product01 = new Product('1', 'Product 01', 10);
+    const product02 = new Product('2', 'Product 02', 20);
+    const products = [product01, product02];
+
+    ProductService.incresePrice(products, 100);
+    expect(product01.price).toBe(20);
+    expect(product02.price).toBe(40);
+  });
+});
