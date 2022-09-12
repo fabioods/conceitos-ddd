915f551dd001d9710102b051431450203e6c5d56
Fabio dos santos <fah_ds@live.com>
2022-09-12 20:01:04 -0300

    feat: add order service

---
+++ src/service/order.service.spec.ts
@@ -0,0 +1,18 @@
+import { Order } from '../entity/order';
+import { OrderItem } from '../entity/orderItem';
+import { OrderService } from './orderService';
+
+describe('Order service test unit tests', () => {
+  it('should calculate the total price of all orders', () => {
+    const orderItem01 = new OrderItem('1', 'Product 01', 10, 2); // 20
+    const orderItem02 = new OrderItem('2', 'Product 02', 20, 1); // 20
+    const orderItem03 = new OrderItem('3', 'Product 03', 30, 3); // 90
+
+    const order01 = new Order('1', '1', [orderItem01, orderItem02]);
+    const order02 = new Order('2', '2', [orderItem03]);
+
+    const orders = [order01, order02];
+    const total = OrderService.calculateTotal(orders);
+    expect(total).toBe(130);
+  });
+});
