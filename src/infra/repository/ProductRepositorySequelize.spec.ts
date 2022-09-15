ab12c2e1a044d0fb210d54ef4db1347a5c6215df
Fabio dos santos <fah_ds@live.com>
2022-09-15 19:16:32 -0300

    feat: add product repository

---
+++ src/infra/repository/ProductRepositorySequelize.spec.ts
@@ -0,0 +1,70 @@
+import { Sequelize } from 'sequelize-typescript';
+import { Product } from '../../domain/entity/product';
+import { ProductModel } from '../db/sequelize/model/Product.model';
+import { ProductRepositorySequelize } from './ProductRepositorySequelize';
+
+describe('Product Repository Sequelize Test', () => {
+  let sequelize: Sequelize;
+  beforeEach(async () => {
+    sequelize = new Sequelize({
+      dialect: 'sqlite',
+      storage: ':memory:',
+      logging: false,
+      sync: { force: true },
+    });
+    sequelize.addModels([ProductModel]);
+    await sequelize.sync();
+  });
+
+  afterEach(async () => {
+    await sequelize.close();
+  });
+
+  it('should create a new product', async () => {
+    const productRepository = new ProductRepositorySequelize();
+    const product = new Product('1', 'Product 1', 10.0);
+    await productRepository.create(product);
+    const productModel = await ProductModel.findByPk(product.id);
+    expect(productModel).not.toBeNull();
+    expect(productModel?.id).toBe('1');
+  });
+
+  it('should update a product', async () => {
+    const productRepository = new ProductRepositorySequelize();
+    const product = new Product('1', 'Product 1', 10.0);
+    await productRepository.create(product);
+    product.changeName('Product 1 Updated');
+    product.changePrice(20.0);
+    await productRepository.update(product);
+    const productModel = await ProductModel.findByPk(product.id);
+    expect(productModel).not.toBeNull();
+    expect(productModel?.id).toBe('1');
+    expect(productModel?.name).toBe('Product 1 Updated');
+  });
+
+  it('should find all products', async () => {
+    const productRepository = new ProductRepositorySequelize();
+    const product1 = new Product('1', 'Product 1', 10.0);
+    const product2 = new Product('2', 'Product 2', 20.0);
+    await productRepository.create(product1);
+    await productRepository.create(product2);
+    const products = await productRepository.findAll();
+    expect(products).not.toBeNull();
+    expect(products.length).toBe(2);
+  });
+
+  it('should find a product by id', async () => {
+    const productRepository = new ProductRepositorySequelize();
+    const product = new Product('1', 'Product 1', 10.0);
+    await productRepository.create(product);
+    const productFound = await productRepository.find(product.id);
+    expect(productFound).not.toBeNull();
+    expect(productFound.id).toBe('1');
+  });
+
+  it('should throw an error when try to find a product by id', async () => {
+    const productRepository = new ProductRepositorySequelize();
+    const errorExpect = 'Product not found';
+    await expect(productRepository.find('1')).rejects.toThrowError(errorExpect);
+  });
+});
---
+++ src/infra/repository/ProductRepositorySequelize.ts
@@ -0,0 +1,35 @@
+import { Product } from '../../domain/entity/product';
+import { ProductRepositoryInterface } from '../../domain/repository/productRepositoryInterface';
+import { ProductModel } from '../db/sequelize/model/Product.model';
+
+export class ProductRepositorySequelize implements ProductRepositoryInterface {
+  async find(id: string): Promise<Product> {
+    const product = await ProductModel.findByPk(id);
+    if (!product) {
+      throw new Error('Product not found');
+    }
+    return new Product(product.id, product.name, product.price);
+  }
+
+  async create(entity: Product): Promise<void> {
+    await ProductModel.create({
+      id: entity.id,
+      name: entity.name,
+      price: entity.price,
+    });
+  }
+
+  async update(entity: Product): Promise<void> {
+    await ProductModel.update(
+      { name: entity.name, price: entity.price },
+      { where: { id: entity.id } }
+    );
+  }
+
+  async findAll(): Promise<Product[]> {
+    const products = await ProductModel.findAll();
+    return products.map(
+      product => new Product(product.id, product.name, product.price)
+    );
+  }
+}
